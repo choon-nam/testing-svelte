ac113ac96f0465477583d61e690a08ca2975c481
choon-nam <ooicn@sph.com.sg>
2025-02-25 10:25:59 +0800

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: '/testing-svelte' // Set this dynamically
+		},
+		prerender: {
+			entries: ['*'] // Ensures all pages are pre-rendered
+		}
+	}
+};
+
+export default config;
